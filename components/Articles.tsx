95bc69eebc3d0495a694e29960e0fa6f10803938
michalma <michal.mancewicz@uptimedevelopment.pl>
2021-09-03 13:55:03 +0200

    use swr for infinite scroll

--- components/Articles.tsx
+++ components/Articles.tsx
@@ -1,42 +1,38 @@
 import { Col, Container, Row } from 'react-bootstrap'
-import { ICategoriesData, IDataItem } from 'api/articles/types'
-import { useEffect, useRef, useState } from 'react'
+import useSWR, { useSWRInfinite } from 'swr'
 
+import { ICategoriesData } from 'api/articles/types'
 import Image from 'next/image'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import axios from 'axios'
 import { getInitialArticles } from 'api/articles'
-import useSWR from 'swr'
+import { useState } from 'react'
 
 const Articles = () => {
-  const [articles, setArticles] = useState<IDataItem[]>([])
   const [hasMore, setHasMore] = useState(true)
-  const { data } = useSWR('/api/articles', getInitialArticles)
-  const page = useRef(1)
+  const { data: initialData } = useSWR('/api/articles', getInitialArticles)
+  const { size, setSize, data } = useSWRInfinite(
+    (index: number) => {
+      return `https://listapi.aripaev.ee/v1/category/grouped?categories=news,investor&channel_id=aripaev&page=${
+        index + 1
+      }`
+    },
+    (url: string) =>
+      axios.get<ICategoriesData>(url).then(({ data }) => {
+        if (!data.investor.length && !data.news.length) setHasMore(false)
+        return [...data.investor, ...data.news]
+      })
+  )
 
-  const getMoreArticles = async () => {
-    try {
-      const { data } = await axios.get<ICategoriesData>(
-        `https://listapi.aripaev.ee/v1/category/grouped?categories=news,investor&channel_id=aripaev&page=${page.current}`
-      )
-
-      if (!data.investor.length && !data.news.length) setHasMore(false)
-
-      page.current += 1
-      setArticles((articles) => [...articles, ...data.investor, ...data.news])
-    } catch (error) {
-      console.log(error)
-    }
+  const infiniteData = data?.reduce((acc, val) => [...acc, ...val], []) || []
+  const handleNext = () => {
+    setSize && size && setSize(size + 1)
   }
-
-  useEffect(() => {
-    getMoreArticles()
-  }, [])
 
   return (
     <Container>
       <Row>
-        {data?.slice(0, 1).map((item) => (
+        {initialData?.slice(0, 1).map((item) => (
           <Col md={8} key={item.uuid}>
             <div className='articles-image-container'>
               <Image
@@ -53,7 +49,7 @@ const Articles = () => {
         ))}
         <Col md={4}>
           <Row>
-            {data?.slice(1, 3).map((item) => (
+            {initialData?.slice(1, 3).map((item) => (
               <Col xs={6} md={12} key={item.uuid}>
                 <div className='articles-image-container'>
                   <Image
@@ -74,14 +70,14 @@ const Articles = () => {
         </Col>
       </Row>
       <InfiniteScroll
-        dataLength={articles.length}
-        next={getMoreArticles}
+        dataLength={infiniteData.length}
+        next={handleNext}
         hasMore={hasMore}
         loader={<h3> Loading...</h3>}
         endMessage={<h4>Nothing more to show</h4>}
       >
         <Row>
-          {data?.slice(3).map((item) => (
+          {initialData?.slice(3).map((item) => (
             <Col
               xs={6}
               md={12}
@@ -104,11 +100,11 @@ const Articles = () => {
               </div>
             </Col>
           ))}
-          {articles.map((item) => (
+          {infiniteData.map((item) => (
             <Col
               xs={6}
               md={12}
-              key={item.uuid + page.current}
+              key={item.uuid}
               className='d-flex flex-sm-column flex-md-row mb-4'
             >
               <div className='articles-image-container small'>
