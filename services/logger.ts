6a02ed53de6d98f8970d35f52c4da998a0afb75b
michalma <michal.mancewicz@uptimedevelopment.pl>
2021-10-04 14:21:43 +0200

    fix console errors in production

--- services/logger.ts
+++ services/logger.ts
@@ -2,11 +2,12 @@ import {
   CloudWatchLogsClient,
   CreateLogGroupCommand,
   CreateLogStreamCommand,
+  DescribeLogGroupsCommand,
   DescribeLogStreamsCommand,
   PutLogEventsCommand,
 } from '@aws-sdk/client-cloudwatch-logs'
 
-const logGroupName = 'next-test-task-log-goup'
+const logGroupName = 'next-test-task-log-goup3'
 const logStreamName = `${
   new Date().toISOString().split('T')[0]
 }/next-test-task-log-stream`
@@ -20,45 +21,63 @@ const client = new CloudWatchLogsClient({
 })
 
 const createLogGroup = async () => {
-  await client.send(new CreateLogGroupCommand({ logGroupName }))
+  try {
+    const data = await client.send(
+      new DescribeLogGroupsCommand({ logGroupNamePrefix: logGroupName })
+    )
+    const isGroupNotFound = data.logGroups?.length === 0
+    if (isGroupNotFound) {
+      await client.send(new CreateLogGroupCommand({ logGroupName }))
+    }
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 const createLogStream = async () => {
-  await client.send(
-    new CreateLogStreamCommand({
-      logStreamName,
-      logGroupName,
-    })
-  )
+  try {
+    const data = await client.send(
+      new DescribeLogStreamsCommand({
+        logGroupName,
+        logStreamNamePrefix: logStreamName,
+      })
+    )
+    const isStreamNotFound = data.logStreams?.length === 0
+    if (isStreamNotFound) {
+      await client.send(
+        new CreateLogStreamCommand({
+          logStreamName,
+          logGroupName,
+        })
+      )
+    }
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 const sendLogEvent = async (message: string) => {
-  const describeLogStreams = new DescribeLogStreamsCommand({ logGroupName })
-  const res = await client.send(describeLogStreams)
-  const currentLogStream = res.logStreams?.find(
-    (el) => el.logStreamName === logStreamName
-  )
+  try {
+    const describeLogStreams = new DescribeLogStreamsCommand({ logGroupName })
+    const res = await client.send(describeLogStreams)
+    const currentLogStream = res.logStreams?.find(
+      (el) => el.logStreamName === logStreamName
+    )
 
-  const logEvents = new PutLogEventsCommand({
-    logGroupName,
-    logStreamName: currentLogStream?.logStreamName,
-    logEvents: [{ timestamp: Date.now(), message }],
-    sequenceToken: currentLogStream?.uploadSequenceToken,
-  })
-  await client.send(logEvents)
+    const logEvents = new PutLogEventsCommand({
+      logGroupName,
+      logStreamName: currentLogStream?.logStreamName,
+      logEvents: [{ timestamp: Date.now(), message }],
+      sequenceToken: currentLogStream?.uploadSequenceToken,
+    })
+    await client.send(logEvents)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 export const logger = async (message: string) => {
-  try {
-    await createLogGroup()
-    await createLogStream()
-    await sendLogEvent(message)
-  } catch {
-    try {
-      await createLogStream()
-      await sendLogEvent(message)
-    } catch {
-      await sendLogEvent(message)
-    }
-  }
+  await createLogGroup()
+  await createLogStream()
+  await sendLogEvent(message)
 }
